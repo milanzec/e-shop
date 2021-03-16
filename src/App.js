8dd4d6bec0877faf2a95bf2b626e64529f95e53f
Your Name <you@example.com>
2021-03-16 01:15:40 +0100

    card not finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { commerce } from './lib/commerce';
 
-import { Products, Navbar } from './components';
+import { Products, Navbar, Cart } from './components';
 
 const App = () => {
   const [products, setProducts] = useState([]);
@@ -27,13 +27,13 @@ const App = () => {
     fetchCart();
   }, []);
 
-  console.log(products);
   console.log(cart);
 
   return (
     <div>
       <Navbar cartItemsTotal={cart.total_items} />
-      <Products products={products} addToCart={handleAddToCart} />
+      {/*<Products products={products} addToCart={handleAddToCart} />*/}
+      <Cart cart={cart} />
     </div>
   );
 };
